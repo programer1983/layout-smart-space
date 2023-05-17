f1ab0de871014df7aba69846a3c9b6a23409f30b
programer1983 <programer972@gmail.com>
2023-05-17 12:39:23 +0300

    create main block

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.slider-main-block', {
+    loop: true,
+  
+    navigation: {
+      nextEl: '.body-main-block__arrow.swiper-button-next',
+      prevEl: '.body-main-block__arrow.swiper-button-prev',
+    },
+  });
